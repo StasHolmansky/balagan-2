a6e9a81a940682bf5b267cf41fa8c4e12ef8716b
LoCoCb01 <goluzin.dima008@mail.ru>
2023-05-25 17:10:36 +0500

    added

--- js/client.js
+++ js/client.js
@@ -24,4 +24,28 @@ function onTabClick(item) {
     });
 }
 
-document.querySelector('.tabs__nav-btn').click();
+let fields = document.querySelectorAll('.field__file');
+        Array.prototype.forEach.call(fields, function (input) {
+          let label = input.nextElementSibling,
+            labelVal = label.querySelector('.field__file-fake').innerText;
+      
+          input.addEventListener('change', function (e) {
+            let countFiles = '';
+            if (this.files && this.files.length >= 1)
+              countFiles = this.files.length;
+      
+            if (countFiles)
+              label.querySelector('.field__file-fake').innerText = 'Выбрано файлов: ' + countFiles;
+            else
+              label.querySelector('.field__file-fake').innerText = labelVal;
+    });
+})
+
+
+
+        
+
+
+
+
+
